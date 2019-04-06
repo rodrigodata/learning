501ab3174a4729e2aaa21f5a7cf94a2d61c9bc0e
Rodrigo Andrade <warodrigoandrade@gmail.com>
2019-04-06 19:54:37 -0300

    WIP

--- pyhton/variables/variables.py
+++ pyhton/variables/variables.py
@@ -1,2 +1,14 @@
-n = 10
-print(n)
+price = 10 #int
+rating = 4.9 #float
+name = "Rodrigo" #string
+is_published = False #boolean
+
+# input from terminal
+### Get name of user from the terminal
+name = input('What is your name? ')
+#print('Hi ' + name)
+
+### Get the favorite color of user from the terminal
+favorite_color = input('What is your favorite color, ' + name + '? ')
+print(name + ' likes ' + favorite_color)
+
