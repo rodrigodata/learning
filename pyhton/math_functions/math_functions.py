5ef815426d17be844d959000541b5def89984e3b
Rodrigo Andrade <warodrigoandrade@gmail.com>
2019-04-07 15:28:58 -0300

    WIP

---
+++ pyhton/math_functions/math_functions.py
@@ -0,0 +1,20 @@
+# round
+x = 2.4
+print(round(x)) # 2
+w = 4.6
+print(round(w)) # 5
+
+# absolute
+y = -1 
+
+print(abs(y)) # 1
+
+
+# MATH MODULE
+import math
+
+# ceil
+print(math.ceil(2.2)) # 3
+
+# floor
+print(math.floor(2.9)) # 2
