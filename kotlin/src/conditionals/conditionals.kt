8f1d306e79e910604fb1505d18990ebd2579cd77
Rodrigo Andrade <warodrigoandrade@gmail.com>
2019-07-20 00:08:36 -0300

    Estudo de Kotlin.

--- kotlin/src/conditionals/conditionals.kt
+++ kotlin/src/conditionals/conditionals.kt
@@ -1,6 +1,8 @@
 package conditionals
 
 fun main(args: Array<String>) {
+    // IF, ELSE IF, ELSE
+    println("!! IF, ELSE IF, ELSE !!")
     val age: Int = 28
     if(age < 18){
         println("You can`t register")
@@ -11,4 +13,24 @@ fun main(args: Array<String>) {
     else {
         println("You are good to go!")
     }
+
+    println("----------------------")
+
+    // WHEN
+    println("!! WHEN !!")
+    val mode: Int = 10
+
+    when(mode){
+        1 -> println("the mode is lazy.")
+        2 -> {
+            println("the mode is 2")
+            println("So the mode is busy.")
+        }
+        3 -> println("The mode is super productive")
+        else -> {
+            println("Unknown mode.")
+        }
+    }
+
+    println("----------------------")
 }
